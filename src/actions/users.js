0d2758fad9ecc680e276d82fda080446823dc1ba
Janet Leon <leon.irenejanet@gmail.com>
2018-11-26 09:48:21 -0800

    add access allow origin header

--- src/actions/users.js
+++ src/actions/users.js
@@ -8,7 +8,8 @@ export const registerUser = user => dispatch => {
     return fetch(`${API_BASE_URL}/users`, {
         method: 'POST',
         headers: {
-            'content-type': 'application/json'
+            'content-type': 'application/json',
+            'Access-Control-Allow-Origin': '*'
         },
         body: JSON.stringify(user)
     })
