6573856e3884a9fe02e0fca072b57b7b8257fa5e
Janet Leon <leon.irenejanet@gmail.com>
2018-11-26 19:45:40 -0800

    adds background image and background to play area

--- src/components/playarea.js
+++ src/components/playarea.js
@@ -3,6 +3,7 @@ import{connect} from 'react-redux';
 import {Redirect} from 'react-router-dom';
 import HeaderBar from './header-bar';
 import './css/playArea.css'
+import { required } from '../validators';
 
 export class PlayArea extends React.Component{
 
@@ -14,9 +15,13 @@ export class PlayArea extends React.Component{
   return(
     <React.Fragment>
        <HeaderBar />
-       <section className="play-page">
+        <section id='#play-area' className="play-page">
        <h1>helllooooo</h1>
+       <section className='houses'>
+       <img src={require('./images/houses.png')} alt="houses"></img>
        </section>
+       </section> 
+       
     </React.Fragment>
   )
   }
