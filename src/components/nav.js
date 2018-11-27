9d8557d5278b0e75bc3c0db4e41e1a3a4b875a10
Janet Leon <leon.irenejanet@gmail.com>
2018-11-26 16:10:24 -0800

    user able to log out

--- src/components/nav.js
+++ src/components/nav.js
@@ -1,63 +1,70 @@
 import React from 'react';
-import {connect} from 'react-redux'
-import {fetchProtectedData} from '../actions/protected-data';
+import { connect } from 'react-redux';
+import { Link } from 'react-router-dom';
+import { clearAuth } from '../actions/auth';
+import { clearAuthToken } from '../local-storage';
 
-export class Navigation extends React.Component{
-  componentDidMount(){
-    console.log(this.props.loggedIn, 'nav 7');
-  }
+export class Navigation extends React.Component {
+	logOut() {
+		this.props.dispatch(clearAuth());
+		clearAuthToken();
+	}
 
+	displayLoggedOut = () => {
+		return (
+			<section className="navigation-bar">
+				<h1>
+					<a href="#home">Learning Spanish</a>
+				</h1>
+				<nav className="navigation-bar-nav">
+					<ul className="navLinks">
+						<li>
+							<a href="#register">Register</a>
+						</li>
+						<li>
+							<a href="#login">Login</a>
+						</li>
+					</ul>
+				</nav>
+			</section>
+		);
+	};
 
+	displayLoggedIn = () => {
+		return (
+			<section className="navigation-bar">
+				<h1>
+					<a href="#home">Learning Spanish</a>
+				</h1>
+				<nav className="navigation-bar-nav">
+					<ul className="navLinks">
+						<li>
+							<a href="#Dashboard">Dashboard</a>
+						</li>
+						<li>
+						<a href='#home' onClick={() => this.logOut()}>Logout</a>
+						</li>
+					</ul>
+				</nav>
+			</section>
+		);
+	};
 
-displayLoggedOut = ()=>{
-return (
-  <section className="navigation-bar">
-  <h1><a href='#home'>Learning Spanish</a></h1>
-   <nav className='navigation-bar-nav'>
-   
-   <ul className='navLinks'>
-   <li><a href='#register'>Register</a></li>
-   <li> 
-   <a href='#login'>Login</a> 
- </li>
-   </ul>
-   </nav>
-   </section>
-);
+	render() {
+		return (
+			<React.Fragment>
+				{/* {this.props.loggedIn===null? this.displayLoggedOut(): } */}
+				{
+					this.props.loggedIn ? this.displayLoggedIn() :
+					this.displayLoggedOut()}
+			</React.Fragment>
+		);
+	}
+}
+
+const mapStateToProps = (state) => {
+	return {
+		loggedIn: state.auth.currentUser !== null
+	};
 };
-
-displayLoggedIn=()=>{
-return(
-  <section className="navigation-bar">
-  <h1><a href='#home'>Learning Spanish</a></h1>
-   <nav className='navigation-bar-nav'>
-   
-   <ul className='navLinks'>
-   <li><a href='#register'>Dashboard</a></li>
-   <li> 
-   <a href='#login'>Logout</a> 
- </li>
-   </ul>
-   </nav>
-   </section>
-);
-}
-
-  render(){
-    console.log(this.props.loggedIn,49);
-
-  return(
-<React.Fragment>
-  {/* {this.props.loggedIn===null? this.displayLoggedOut(): } */}
-  {this.props.loggedIn?  this.displayLoggedIn(): this.displayLoggedOut()}
-</React.Fragment>
-  )
-  }
-}
-
-const mapStateToProps=(state)=>{
-return{
-  loggedIn: state.auth.currentUser !== null
-}
-}
 export default connect(mapStateToProps)(Navigation);
