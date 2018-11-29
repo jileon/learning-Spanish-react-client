cba4ff280dd7c00515c4622fb5adb36c3547f79e
Aaron Whitehead <whitehead.aaron@live.com>
2018-11-29 11:09:25 -0700

    css

--- src/components/login-landing-page.js
+++ src/components/login-landing-page.js
@@ -1,21 +1,23 @@
-import React from 'react';
-import LoginForm from './login-form';
+import React from "react";
+import LoginForm from "./login-form";
 
-
-export default function LoginPage(props){
-  return(
+export default function LoginPage(props) {
+  return (
     <React.Fragment>
-      <div id='login'></div>
-    <section  className='page-container user-page'>
-    <section className='user-area'>
-        {/* <h3>Placeholder</h3> */}
-        <img className='login-img' alt="login" src={require('./images/landing-people.png') }></img>
+      <div id="login" />
+      <section className="page-container user-page login-user">
+        <section className="user-area no-show">
+          {/* <h3>Placeholder</h3> */}
+          <img
+            className="login-img"
+            alt="login"
+            src={require("./images/landing-people.png")}
+          />
+        </section>
+        <section className="user-area">
+          <LoginForm />
+        </section>
       </section>
-     <section className='user-area'>
-      <LoginForm/>
-      </section>
-      
-    </section>
     </React.Fragment>
-  )
+  );
 }
