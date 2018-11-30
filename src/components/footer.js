7ac7045c93bd86146a586b356311910b75d26381
Janet Leon <leon.irenejanet@gmail.com>
2018-11-30 11:55:38 -0800

    fixes feedback font size

--- src/components/footer.js
+++ src/components/footer.js
@@ -4,7 +4,7 @@ export default function Footer(){
   return(
     <footer>
       Created by <a href='https://aaron-whitehead.herokuapp.com'>Aaron Whitehead</a> and <a href='https://iamjanetleon.com/'>Janet Leon</a>
-      <a href='https://github.com/thinkful-ei24/aaron-janet-spaced-repetition-client'><i class="fab fa-github"></i></a>
+      <a href='https://github.com/thinkful-ei24/aaron-janet-spaced-repetition-client'><i className="fab fa-github"></i></a>
     </footer>
   )
 };
