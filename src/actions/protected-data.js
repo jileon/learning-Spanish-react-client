9ce1203141ab7035eca6035358cbc7a3fae8b73a
Janet Leon <ajleon00@gmail.com>
2019-01-06 17:46:39 -0800

    takes out console logs

--- src/actions/protected-data.js
+++ src/actions/protected-data.js
@@ -1,98 +1,86 @@
-import {API_BASE_URL} from '../config';
-import {normalizeResponseErrors} from './utils';
+import { API_BASE_URL } from '../config';
+import { normalizeResponseErrors } from './utils';
 import axios from 'axios';
-
 
 export const FETCH_PROTECTED_DATA_SUCCESS = 'FETCH_PROTECTED_DATA_SUCCESS';
 export const fetchProtectedDataSuccess = data => ({
-    type: FETCH_PROTECTED_DATA_SUCCESS,
-    data
+  type: FETCH_PROTECTED_DATA_SUCCESS,
+  data
 });
 
 export const FETCH_PROTECTED_DATA_ERROR = 'FETCH_PROTECTED_DATA_ERROR';
 export const fetchProtectedDataError = error => ({
-    type: FETCH_PROTECTED_DATA_ERROR,
-    error
+  type: FETCH_PROTECTED_DATA_ERROR,
+  error
 });
 
-
-
-
 export const CORRECT_ANSWER = 'CORRECT_ANSWER';
-export const correctAnswer= ()=>({
-type: CORRECT_ANSWER,
-})
-
+export const correctAnswer = () => ({
+  type: CORRECT_ANSWER
+});
 
 export const WRONG_ANSWER = 'WRONG_ANSWER';
-export const wrongAnswer= ()=>({
-type: WRONG_ANSWER,
-})
+export const wrongAnswer = () => ({
+  type: WRONG_ANSWER
+});
 
-export const RESET_FEEDBACK = "RESET_FEEDBACK";
-export const resetFeedback = ()=>({
-    type:RESET_FEEDBACK
-})
+export const RESET_FEEDBACK = 'RESET_FEEDBACK';
+export const resetFeedback = () => ({
+  type: RESET_FEEDBACK
+});
 
-
-export const fetchStats= ()=>(dispatch, getState)=>{
-    const authToken = getState().auth.authToken;
-    console.log(authToken);
-    const userId = getState().auth.currentUser.id;
-    const username = getState().auth.currentUser.username;
-    const body = {username, userId};
-    console.log(body);
-   return fetch(`${API_BASE_URL}/stats`, {
-        method: 'POST',
-        body: JSON.stringify(body),
-        headers: {
-            Authorization: `Bearer ${authToken}`,
-            'content-type': 'application/json'
-        }
+export const fetchStats = () => (dispatch, getState) => {
+  const authToken = getState().auth.authToken;
+  const userId = getState().auth.currentUser.id;
+  const username = getState().auth.currentUser.username;
+  const body = { username, userId };
+  return fetch(`${API_BASE_URL}/stats`, {
+    method: 'POST',
+    body: JSON.stringify(body),
+    headers: {
+      Authorization: `Bearer ${authToken}`,
+      'content-type': 'application/json'
+    }
+  })
+    .then(res => normalizeResponseErrors(res))
+    .then(res => res.json())
+    .then(res => {
+      dispatch(fetchProtectedData());
     })
-        .then(res => normalizeResponseErrors(res))
-        .then(res => res.json())
-        .then((res) =>{
-            dispatch(fetchProtectedData())
-        } )
-        .catch(err => {
-            dispatch(fetchProtectedDataError(err));
-        });
+    .catch(err => {
+      dispatch(fetchProtectedDataError(err));
+    });
 };
 
+export const fetchProtectedData = () => (dispatch, getState) => {
+  const authToken = getState().auth.authToken;
 
-export const fetchProtectedData =() => (dispatch, getState) => {
-    const authToken = getState().auth.authToken;
-
-    return fetch(`${API_BASE_URL}/stats`, {
-        method: 'GET',
-        // body: JSON.stringify(body),
-        headers: {
-            // Provide our auth token as credentials
-            Authorization: `Bearer ${authToken}`
-        }
-    })
-        .then(res => normalizeResponseErrors(res))
-        .then(res => res.json())
-        .then((res) => dispatch(fetchProtectedDataSuccess(res)))
-        .catch(err => {
-            dispatch(fetchProtectedDataError(err));
-        });
+  return fetch(`${API_BASE_URL}/stats`, {
+    method: 'GET',
+    // body: JSON.stringify(body),
+    headers: {
+      // Provide our auth token as credentials
+      Authorization: `Bearer ${authToken}`
+    }
+  })
+    .then(res => normalizeResponseErrors(res))
+    .then(res => res.json())
+    .then(res => dispatch(fetchProtectedDataSuccess(res)))
+    .catch(err => {
+      dispatch(fetchProtectedDataError(err));
+    });
 };
 
-
-export const updateStats= (id, correct, incorrect)=>(dispatch, getState)=>{
-    const authToken = getState().auth.authToken;
-    const data = getState().protectedData.data;
-    data.correct=correct;
-    data.incorrect=incorrect;
-    return axios(`${API_BASE_URL}/stats/${id}`, {
-        method: 'PUT',
-        data:data ,
-        headers: {
-            Authorization: `Bearer ${authToken}`,
-
-        }
-    })
-    .catch(err=>console.log(err))
-}
+export const updateStats = (id, correct, incorrect) => (dispatch, getState) => {
+  const authToken = getState().auth.authToken;
+  const data = getState().protectedData.data;
+  data.correct = correct;
+  data.incorrect = incorrect;
+  return axios(`${API_BASE_URL}/stats/${id}`, {
+    method: 'PUT',
+    data: data,
+    headers: {
+      Authorization: `Bearer ${authToken}`
+    }
+  }).catch(err => console.log(err));
+};
--- src/components/registration-form.js
+++ src/components/registration-form.js
@@ -1,76 +1,72 @@
 import React from 'react';
-import {Field, reduxForm, focus} from 'redux-form';
-import {registerUser} from '../actions/users';
-import {login} from '../actions/auth';
+import { Field, reduxForm, focus } from 'redux-form';
+import { registerUser } from '../actions/users';
+import { login } from '../actions/auth';
 import Input from './input';
-import {fetchStats} from '../actions/protected-data';
-import {required, nonEmpty, matches, length, isTrimmed} from '../validators';
+import { fetchStats } from '../actions/protected-data';
+import { required, nonEmpty, matches, length, isTrimmed } from '../validators';
 import './css/form.css';
-const passwordLength = length({min: 10, max: 72});
+const passwordLength = length({ min: 10, max: 72 });
 const matchesPassword = matches('password');
 
-
 export class RegistrationForm extends React.Component {
-    onSubmit(values) {
-        const {username, password, firstName, lastName} = values;
-        const user = {username, password, firstName, lastName};
-        return this.props
-            .dispatch(registerUser(user))
-            .then((user) => this.props.dispatch(login(username, password)))
-            .then(()=>{
-                console.log('fetchStats', 20)
-                this.props.dispatch(fetchStats());
-            })
-            
-    }
+  onSubmit(values) {
+    const { username, password, firstName, lastName } = values;
+    const user = { username, password, firstName, lastName };
+    return this.props
+      .dispatch(registerUser(user))
+      .then(user => this.props.dispatch(login(username, password)))
+      .then(() => {
+        this.props.dispatch(fetchStats());
+      });
+  }
 
-    render() {
-        return (
-            <form
-                className="login-form register-form"
-                onSubmit={this.props.handleSubmit(values =>
-                    this.onSubmit(values)
-                )}>
-                <label htmlFor="firstName">First name</label>
-                <Field component={Input} type="text" name="firstName" />
-                <label htmlFor="lastName">Last name</label>
-                <Field component={Input} type="text" name="lastName" />
-                <label htmlFor="username">Username</label>
-                <Field
-                
-                    component={Input}
-                    type="text"
-                    name="username"
-                    validate={[required, nonEmpty, isTrimmed]}
-                />
-             
-                <label htmlFor="password">Password</label>
-                <Field
-                    component={Input}
-                    type="password"
-                    name="password"
-                    validate={[required, passwordLength, isTrimmed]}
-                />
-                <label htmlFor="passwordConfirm">Confirm password</label>
-                <Field
-                    component={Input}
-                    type="password"
-                    name="passwordConfirm"
-                    validate={[required, nonEmpty, matchesPassword]}
-                />
-                <button
-                    type="submit"
-                    disabled={this.props.pristine || this.props.submitting}
-                    className="play-button button2">
-                    Register
-                </button>
-            </form>
-        );
-    }
+  render() {
+    return (
+      <form
+        className="login-form register-form"
+        onSubmit={this.props.handleSubmit(values => this.onSubmit(values))}
+      >
+        <label htmlFor="firstName">First name</label>
+        <Field component={Input} type="text" name="firstName" />
+        <label htmlFor="lastName">Last name</label>
+        <Field component={Input} type="text" name="lastName" />
+        <label htmlFor="username">Username</label>
+        <Field
+          component={Input}
+          type="text"
+          name="username"
+          validate={[required, nonEmpty, isTrimmed]}
+        />
+
+        <label htmlFor="password">Password</label>
+        <Field
+          component={Input}
+          type="password"
+          name="password"
+          validate={[required, passwordLength, isTrimmed]}
+        />
+        <label htmlFor="passwordConfirm">Confirm password</label>
+        <Field
+          component={Input}
+          type="password"
+          name="passwordConfirm"
+          validate={[required, nonEmpty, matchesPassword]}
+        />
+        <button
+          type="submit"
+          disabled={this.props.pristine || this.props.submitting}
+          className="play-button button2"
+        >
+          Register
+        </button>
+      </form>
+    );
+  }
 }
 
 export default reduxForm({
-    form: 'registration',
-    onSubmitFail: (errors, dispatch) =>
-        dispatch(focus('registration', Object.keys(errors)[0]))
+  form: 'registration',
+  onSubmitFail: (errors, dispatch) =>
+    dispatch(focus('registration', Object.keys(errors)[0]))
 })(RegistrationForm);
