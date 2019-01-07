d80101689800939f71144213bf1802bbcf4d7c9b
Janet Leon <ajleon00@gmail.com>
2019-01-07 00:06:42 -0800

    fixes validator overlfow

--- src/components/nav.js
+++ src/components/nav.js
@@ -3,72 +3,76 @@ import { connect } from 'react-redux';
 import { Link } from 'react-router-dom';
 import { clearAuth } from '../actions/auth';
 import { clearAuthToken } from '../local-storage';
-import {resetFeedback} from '../actions/protected-data';
+import { resetFeedback } from '../actions/protected-data';
 
 export class Navigation extends React.Component {
-	logOut() {
-		this.props.dispatch(resetFeedback());
-		this.props.dispatch(clearAuth());
-		clearAuthToken();
-	}
+  logOut() {
+    this.props.dispatch(resetFeedback());
+    this.props.dispatch(clearAuth());
+    clearAuthToken();
+  }
 
-	displayLoggedOut = () => {
-		return (
-			<section className="navigation-bar">
-				<h1>
-					<a href="#home">Learning Spanish</a>
-				</h1>
-				<nav className="navigation-bar-nav">
-		
-					<ul className="navLinks">
-						<li>
-							<a href="#register">Register</a>
-						</li>
-						<li>
-							<a href="#login">Login</a>
-						</li>
-					</ul>
-				</nav>
-			</section>
-		);
-	};
+  displayLoggedOut = () => {
+    return (
+      <section className="navigation-bar">
+        <div className="logo">
+          <h1>
+            <a href="#home">Learning Spanish</a>
+          </h1>
+        </div>
+        <nav className="navigation-bar-nav">
+          <ul className="navLinks">
+            <li>
+              <a href="#register">Register</a>
+            </li>
+            <li>
+              <a href="#login">Login</a>
+            </li>
+          </ul>
+        </nav>
+      </section>
+    );
+  };
 
-	displayLoggedIn = () => {
-		return (
-			<section className="navigation-bar">
-				<h1>
-				<Link to="/dashboard">Learning Spanish</Link>
-				</h1>
-				<nav className="navigation-bar-nav">
-					<ul className="navLinks">
-						<li>
-							<Link to="/dashboard">Dashboard</Link>
-						</li>
-						<li>
-						<Link to="/play">Play</Link>
-						</li>
-						<li>
-						<a href='#home' onClick={() => this.logOut()}>Logout</a>
-						</li>
-					</ul>
-				</nav>
-			</section>
-		);
-	};
+  displayLoggedIn = () => {
+    return (
+      <section className="navigation-bar">
+        <div className="logo">
+          <h1>
+            <Link to="/dashboard">Learning Spanish</Link>
+          </h1>
+        </div>
+        <nav className="navigation-bar-nav">
+          <ul className="navLinks">
+            <li>
+              <Link to="/dashboard">Dashboard</Link>
+            </li>
+            <li>
+              <Link to="/play">Play</Link>
+            </li>
+            <li>
+              <a href="#home" onClick={() => this.logOut()}>
+                Logout
+              </a>
+            </li>
+          </ul>
+        </nav>
+      </section>
+    );
+  };
 
-	render() {
-		return (
-			<React.Fragment>
-				{	this.props.loggedIn ? this.displayLoggedIn() :
-					this.displayLoggedOut()}
-			</React.Fragment>
-		);
-	}
+  render() {
+    return (
+      <React.Fragment>
+        {this.props.loggedIn ? this.displayLoggedIn() : this.displayLoggedOut()}
+      </React.Fragment>
+    );
+  }
 }
 
-const mapStateToProps = (state) => {
-	return {
-		loggedIn: state.auth.currentUser !== null
-	};
+const mapStateToProps = state => {
+  return {
+    loggedIn: state.auth.currentUser !== null
+  };
 };
 export default connect(mapStateToProps)(Navigation);
--- src/validators.js
+++ src/validators.js
@@ -1,19 +1,19 @@
 export const required = value => (value ? undefined : 'Required');
-export const unrequired = value => (value ? undefined : 'Username Required. Demo: username: test');
-export const pwrequired = value => (value ? undefined : 'Password Required. Demo: password: password123');
+export const unrequired = value => (value ? undefined : 'Username Required');
+export const pwrequired = value => (value ? undefined : 'Password Required');
 export const nonEmpty = value =>
-    value.trim() !== '' ? undefined : 'Cannot be empty';
+  value.trim() !== '' ? undefined : 'Cannot be empty';
 export const isTrimmed = value =>
-    value.trim() === value ? undefined : 'Cannot start or end with whitespace';
+  value.trim() === value ? undefined : 'Cannot start or end with whitespace';
 export const length = length => value => {
-    if (length.min && value.length < length.min) {
-        return `Must be at least ${length.min} characters long`;
-    }
-    if (length.max && value.length > length.max) {
-        return `Must be at most ${length.max} characters long`;
-    }
+  if (length.min && value.length < length.min) {
+    return `Must be at least ${length.min} characters long`;
+  }
+  if (length.max && value.length > length.max) {
+    return `Must be at most ${length.max} characters long`;
+  }
 };
 export const matches = field => (value, allValues) =>
-    field in allValues && value.trim() === allValues[field].trim()
-        ? undefined
-        : 'Does not match';
+  field in allValues && value.trim() === allValues[field].trim()
+    ? undefined
+    : 'Does not match';
