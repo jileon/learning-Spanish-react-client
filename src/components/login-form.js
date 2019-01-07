d6a847979053e820679601fd17e0dcb511e7724a
Janet Leon <ajleon00@gmail.com>
2019-01-06 17:24:25 -0800

    gets rid of console log errors for redux form dupe ids

--- src/components/login-form.js
+++ src/components/login-form.js
@@ -1,61 +1,61 @@
 import React from 'react';
-import {Field, reduxForm, focus} from 'redux-form';
+import { Field, reduxForm, focus } from 'redux-form';
 import Input from './input';
-import {login} from '../actions/auth';
-import {nonEmpty, unrequired,pwrequired} from '../validators';
+import { login } from '../actions/auth';
+import { nonEmpty, unrequired, pwrequired } from '../validators';
 
 export class LoginForm extends React.Component {
-    onSubmit(values) {
-        return this.props.dispatch(login(values.username, values.password));
-    }
+  onSubmit(values) {
+    return this.props.dispatch(login(values.username, values.password));
+  }
 
-    render() {
-        let error;
-        if (this.props.error) {
-            error = (
-                <div className="form-error" aria-live="polite">
-                    {this.props.error}
-                </div>
-            );
-        }
-        return (
-            <form
-                className="login-form"
-                onSubmit={this.props.handleSubmit(values =>
-                    this.onSubmit(values)
-                )}>
-                <p>Demo Username: test</p>
-                <p>Demo Password: password123</p>
-                {error}
-                <label htmlFor="username"></label>
-                
-                <Field
-                    component={Input}
-                    placeholder="test"
-                    type="text"
-                    name="username"
-                    id="username"
-                    validate={[unrequired, nonEmpty]}
-                />
-                   <div className='form-separator'></div>
-                <label htmlFor="password"></label>
-                <Field
-                    placeholder="password123"
-                    component={Input}
-                    type="password"
-                    name="password"
-                    id="password"
-                    validate={[pwrequired, nonEmpty]}
-                />
-                <button disabled={this.props.pristine || this.props.submitting} className="play-button button2">
-                    Log in
-                </button>
-            </form>
-        );
+  render() {
+    let error;
+    if (this.props.error) {
+      error = (
+        <div className="form-error" aria-live="polite">
+          {this.props.error}
+        </div>
+      );
     }
+    return (
+      <form
+        className="login-form"
+        onSubmit={this.props.handleSubmit(values => this.onSubmit(values))}
+      >
+        <p>Demo Username: test</p>
+        <p>Demo Password: password123</p>
+        {error}
+        <label htmlFor="username" />
+
+        <Field
+          component={Input}
+          placeholder="test"
+          type="text"
+          name="username-login"
+          validate={[unrequired, nonEmpty]}
+        />
+        <div className="form-separator" />
+        <label htmlFor="password" />
+        <Field
+          placeholder="password123"
+          component={Input}
+          type="password"
+          name="password-login"
+          validate={[pwrequired, nonEmpty]}
+        />
+        <button
+          disabled={this.props.pristine || this.props.submitting}
+          className="play-button button2"
+        >
+          Log in
+        </button>
+      </form>
+    );
+  }
 }
 
 export default reduxForm({
-    form: 'login',
-    onSubmitFail: (errors, dispatch) => dispatch(focus('login', 'username'))
+  form: 'login',
+  onSubmitFail: (errors, dispatch) => dispatch(focus('login', 'username'))
 })(LoginForm);
--- src/components/login-landing-page.js
+++ src/components/login-landing-page.js
@@ -1,16 +1,15 @@
-import React from "react";
-import LoginForm from "./login-form";
+import React from 'react';
+import LoginForm from './login-form';
 
 export default function LoginPage(props) {
   return (
     <React.Fragment>
-     
       <section id="login" className="page-container user-page login-user">
         <section className="user-area no-show">
           <img
             className="login-img"
             alt="login"
-            src={require("./images/landing-people.png")}
+            src={require('./images/landing-people.png')}
           />
         </section>
         <section className="user-area">
